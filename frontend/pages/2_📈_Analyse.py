740d86db4dc6619a6ed49a769bb5aba2c6082311
johmag2 <johan.o.magnusson@me.com>
2024-08-08 21:35:17 +0200

    More segmentation options

--- frontend/pages/2_📈_Analyse.py
+++ frontend/pages/2_📈_Analyse.py
@@ -10,53 +10,96 @@ st.title("Analyse Text")
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
-with st.container():
-    st.write("Select analyzing granulity:")
-    
-    with st.container(border=True):
-        st.checkbox("Seperate by paragraphs",value=True,key="use_para")
-        
-        if not st.session_state['use_para']:
-            st.number_input("Seperate every x word:", key="min_size",value=10, step=1, min_value = 0, disabled=True)
-        else:
-            st.number_input("Set minimum word count:", key="min_size",value=10, step=1, min_value = 0)
-            
-    with st.container(border=True):
-        st.checkbox("Seperate by paragraphs",value=st.session_state["use_para"])
-        
-        if not st.session_state['use_para']:
-            st.number_input("Seperate every x word:", disabled=True)
 
-if st.button('Analyse'):
-    # Initialize the progress bar
-    
-    if 'text_input' in st.session_state:
+if 'text_input' in st.session_state:
+
+    with st.container():
+        st.write("Select analyzing granulity:")
+        option = st.selectbox(
+            "Select how to seperate the segments:",
+            ("Paragraphs", "Sentences", "Words"),
+            placeholder="Select seperation method...",
+        )
         
-        paragraphs = re.split('\n\n|\r\n', st.session_state['text_input'])
-        paragraphs = [i for i in paragraphs if len(i.split()) > st.session_state['min_size']]
-        st.session_state['paragraphs'] = paragraphs
-    
-        progress_text = "Analyzing... Please wait."
-        progress_bar = st.progress(0, text=progress_text)
-        progress_bar.empty()
+        if option == "Paragraphs":
+            st.number_input("Minimum words per paragraph",  key="min_size",value=10, step=1, min_value = 0)
         
-        steps = 1/len(paragraphs)
-        scores = []
+        elif option == "Sentences":
+            st.number_input("How many sentences per segment:",key="sentences_buffer",value=10, step=1, min_value = 0)
+         
+        elif option == "Words":
+            st.number_input("How many words per segment:",key="word_buffer",value=10, step=1, min_value = 0)
         
-        for i,text in enumerate(paragraphs):
-            # Progress bar
-            progress_bar.progress(i*steps, text=progress_text)
-            
-            #work being done
-            tokens = tokenizer.encode(text, return_tensors='pt')
-            result = model(tokens)
-            
-            scores.append(int(torch.argmax(result.logits))+1)
+        else:
+            pass
         
-        progress_bar.progress(100, text=progress_text)
-        
-        st.session_state['scores'] = scores
+        analyze = st.button("Analyse")
+        if analyze:
+            # Initialize the progress bar
                 
-        st.success("Process Completed!")
-    else:
-        st.write("No text inputted")
+            if option == "Paragraphs":
+                paragraphs = re.split('\n\n|\r\n', st.session_state['text_input'])
+                paragraphs = [i for i in paragraphs if len(i.split()) > st.session_state['min_size']]
+            elif option == "Sentences":
+                paragraphs =  re.split(r'(?<=[.!?])\s+', st.session_state['text_input'])
+            elif option == "Words":
+                paragraphs = st.session_state['text_input'].split()
+            else:
+                pass
+            
+            paragraphs = [i for i in paragraphs if len(i)>0]
+                
+            if option == "Sentences":
+                new_para = []
+                buffer = st.session_state['sentences_buffer']
+                    
+                for i in range(len(paragraphs)//buffer):
+                    try:
+                        text = ' '.join(paragraphs[i*buffer:(i+1)*buffer])
+                    except:
+                        text = ' '.join(paragraphs[i*buffer:])
+                        
+                    new_para.append(text)
+                        
+                paragraphs = new_para
+                
+            elif option == "Words":
+                new_para = []
+                buffer = st.session_state['word_buffer']
+                    
+                for i in range(len(paragraphs)//buffer):
+                    try:
+                        text = ' '.join(paragraphs[i*buffer:(i+1)*buffer])
+                    except:
+                        text = ' '.join(paragraphs[i*buffer:])
+                        
+                    new_para.append(text)
+                        
+                paragraphs = new_para
+            
+            st.session_state['paragraphs'] = paragraphs
+
+            progress_text = "Analyzing... Please wait."
+            progress_bar = st.progress(0, text=progress_text)
+            progress_bar.empty()
+            
+            steps = 1/len(paragraphs)
+            scores = []
+            
+            for i,text in enumerate(paragraphs):
+                # Progress bar
+                progress_bar.progress(i*steps, text=progress_text)
+                
+                #work being done
+                tokens = tokenizer.encode(text, return_tensors='pt')
+                result = model(tokens)
+                
+                scores.append(int(torch.argmax(result.logits))+1)
+            
+            progress_bar.progress(100, text=progress_text)
+            
+            st.session_state['scores'] = scores
+                    
+            st.success("Process Completed!")
+else:
+    st.header("No text inputted")
